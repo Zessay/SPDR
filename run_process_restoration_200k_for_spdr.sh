6e32f3ed49dcf44e06fdeb897b4b81377c834a1c
Zessay <zhushuai0403@163.com>
2020-12-15 21:15:14 +0800

    fix import bug

--- run_process_restoration_200k_for_spdr.sh
+++ run_process_restoration_200k_for_spdr.sh
@@ -1,3 +1,3 @@
 python ./tests/codes/test_process_restoration_200k.py \
-  --basename=data/corpus/Restoration-200K/processed_data \
+  --basename=/data/corpus/Restoration-200K/processed_data \
   --tgt_basename=/data/corpus/restoration_200k
